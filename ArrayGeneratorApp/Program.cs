7460e2bbf278ae1fade6f25f844ecf14c68d1c4d
Mustafaalua <202105164@post.au.dk>
2024-03-14 07:17:52 +0100

    Creation

---
+++ ArrayGeneratorApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ArrayGeneratorApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ ArrayGeneratorLibrary/Arrays/IArrayGenerator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayGeneratorLibrary.Arrays
+{
+    /// <summary>
+    /// Interface to change what kind of array we generate doing runtime
+    /// </summary>
+    public interface IArrayGenerator
+    {
+        int[] GetArray(int n, int seed);
+    }
+}
